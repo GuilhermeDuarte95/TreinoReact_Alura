4b8460314c731472d47e8d09c778247f3ad4a498
Guilherme Duarte <guilhermeduarte.informatica@gmail.com>
2024-10-16 12:05:37 -0300

    [FEAT] Adicionando projeto de Treino React da Alura

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Cabecalho from './components/Cabecalho';
+import Container from './components/Container';
+
+function App() {
+  return (
+    <>
+      <Cabecalho/>
+      <Container></Container>
+    </>
+  );
+}
+
+export default App;
